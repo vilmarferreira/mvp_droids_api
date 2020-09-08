3fddc68f3ba8acaf555b847568dddb0e877e25f8
vilmarferreira <ferreiragomes7792@gmail.com>
2020-09-08 18:38:04 -0300

    serializer class to model Contato

---
+++ contato/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Contato
+
+
+class ContatoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contato
+        fields = '__all__'
