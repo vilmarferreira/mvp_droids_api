b92df27eefc25e02436e8193b243fb158d090ef0
vilmarferreira <ferreiragomes7792@gmail.com>
2020-09-09 10:04:06 -0300

    endpoit list states for endereço

--- MVP/routers.py
+++ MVP/routers.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from demanda.views import DemandaViewSet
-
+from endereco.views import EstadoViewSet
 
 ROUTER = routers.SimpleRouter(trailing_slash=False)
 ROUTER.register('demandas',DemandaViewSet)
+ROUTER.register('estados',EstadoViewSet)
--- endereco/views.py
+++ endereco/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import  GenericViewSet
+from endereco.models import Estado
+from endereco.serializers import EstadoSerializer
 
-# Create your views here.
+
+class EstadoViewSet(mixins.RetrieveModelMixin,
+                    mixins.ListModelMixin,
+                   GenericViewSet):
+
+    queryset = Estado.objects.all()
+    serializer_class = EstadoSerializer
